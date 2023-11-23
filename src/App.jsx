817dbfe604789776114ded6cad2cd8268b6fdbe2
sdgamer007 <sohaildua38@gmail.com>
2023-11-23 23:31:17 +0100

    toggle checklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import "./style.css"
 
 function App() {
@@ -15,19 +15,49 @@ function App() {
       ]
     })
   }
-
+  function toggleTodoCompleted(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos?.map((todo) => {
+        if (todo.id === id) return { ...todo, completed }
+        return todo
+      })
+    })
+  }
   return (
-    <div id="new-todo-form">
-      {JSON.stringify(todos)}
-      <label htmlFor="todo-input">New Todo</label>
-      <input
-        type="text"
-        id="todo-input"
-        value={newTodoName}
-        onChange={(e) => setNewTodoName(e.target.value)}
-      />
-      <button onClick={addNewTodo}>Add Todo</button>
-    </div>
+    <React.Fragment>
+      <ul id="list">
+        {todos?.map((todo) => {
+          return (
+            <li key={todo.id} className="list-item">
+              <label className="list-item-label">
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  data-list-item-checkbox
+                  onChange={(e) =>
+                    toggleTodoCompleted(todo.id, e.target.checked)
+                  }
+                />
+                <span data-list-item-text>{todo.name}</span>
+              </label>
+              <button data-button-delete>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+
+      <div id="new-todo-form">
+        {JSON.stringify(todos)}
+        <label htmlFor="todo-input">New Todo</label>
+        <input
+          type="text"
+          id="todo-input"
+          value={newTodoName}
+          onChange={(e) => setNewTodoName(e.target.value)}
+        />
+        <button onClick={addNewTodo}>Add Todo</button>
+      </div>
+    </React.Fragment>
   )
 }
 
