8f722c5808ece8c2755996f28dc7b2be5987d6bb
sdgamer007 <sohaildua38@gmail.com>
2023-11-23 01:17:28 +0100

    generating a new value on click of button for todo list"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react"
+import "./style.css"
+
 function App() {
-  return "HI"
+  const [newTodoName, setNewTodoName] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function addNewTodo() {
+    if (newTodoName === "") return
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { name: newTodoName, completed: false, id: crypto.randomUUID() },
+      ]
+    })
+  }
+
+  return (
+    <div id="new-todo-form">
+      {JSON.stringify(todos)}
+      <label htmlFor="todo-input">New Todo</label>
+      <input
+        type="text"
+        id="todo-input"
+        value={newTodoName}
+        onChange={(e) => setNewTodoName(e.target.value)}
+      />
+      <button onClick={addNewTodo}>Add Todo</button>
+    </div>
+  )
 }
 
 export default App
